2ab898b19a2153ad69592a66a88d37e9ddccbbc6
Vardhan-v <149605800+Vardhan-v@users.noreply.github.com>
2026-05-05 13:46:51 +0530

    Update warehouse_queries.sql

--- data-processing/sql/warehouse_queries.sql
+++ data-processing/sql/warehouse_queries.sql
@@ -1,1 +1,24 @@
-To be updated
+# Warehouse SQL Queries
+
+  ## Aggregation Queries
+
+  ### Aggregates by Date
+  
+SELECT
+    Date,
+    AVG(temperature) AS avg_temp,
+    MAX(temperature) AS max_temp,
+    MIN(temperature) AS min_temp,
+    AVG(wind_speed_mph) AS avg_wind_speed
+FROM weather_raw_silver
+GROUP BY Date
+ORDER BY Date;
+
+### Aggregates by Hour
+
+SELECT
+    Hour,
+    AVG(temperature) AS avg_temp
+FROM weather_raw_silver
+GROUP BY Hour
+ORDER BY Hour;
